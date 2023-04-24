bcf1a54499e5b8bf923ebbfcabcce75d28210287
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-04-24 17:55:25 -0500

    Create level.lua

---
+++ turtles/level.lua
@@ -0,0 +1,17 @@
+a=print b=write c=tonumber d=read e=turtle.dig f=turtle.forward g=turtle.turnLeft h=turtle.turnRight i=function () local j,k=turtle.inspect() return j end l=function () for m=1, 50 do if i() then e() else break end end
+a "level"
+b "x:"
+x=c(d())
+b "y:"
+y=c(d())
+for X=1,x do
+  for Y=1,y do
+    l()
+    f()
+  end
+  if X%2==1 then
+    g() l() f() l()
+  else
+    g() h() f() h()
+  end
+end
